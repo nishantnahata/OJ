ed33a9aa10315b9e28c1b6cf0b1d2b147b6a1489
nishant <nishant.nahata97@gmail.com>
2017-06-23 16:23:51 +0530

    Editor Fixed.

--- submissions/modelForms.py
+++ submissions/modelForms.py
@@ -1,12 +1,23 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django_ace import AceWidget
 
 mode_map={
-    'c':'c_cpp',
-    'cpp':'c_cpp',
-    'java':'java',
-    'python':'python',
+    'c': 'c_cpp',
+    'cpp': 'c_cpp',
+    'java': 'java',
+    'python': 'python',
+    '': 'c_cpp',
 }
+
+LANG_CHOICES = (
+    ('-', '-'),
+    ('cpp', 'cpp'),
+    ('c', 'c'),
+    ('java', 'java'),
+    ('python', 'python'),
+)
+
 
 class EditorForm(forms.Form):
     code = forms.CharField()
@@ -16,3 +27,14 @@ class EditorForm(forms.Form):
         self.fields['code'] = forms.CharField(widget=AceWidget(mode=mode_map[lang],
                                                                theme='daylight'))
 
+
+
+class LangSelect(forms.Form):
+    lang = forms.ChoiceField(choices=LANG_CHOICES)
+
+    def cleaned_lang(self):
+        lang = self.cleaned_data['lang']
+        lang = dict(self.fields['lang'].choices)[lang]
+        if lang == '-':
+            raise ValidationError(u'Language cannot be left blank')
+        return lang
--- submissions/views.py
+++ submissions/views.py
@@ -1,25 +1,46 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .modelForms import EditorForm
-import langs
+from .modelForms import EditorForm, LangSelect
 
 
 class EditorView(View):
     template_name = 'submissions/code_editor.html'
-    form_class = EditorForm
+    form_class1 = EditorForm
+    form_class2 = LangSelect
+    lang = str(None)
 
     def get(self, request):
-        form = self.form_class(request.user.coder.lang, None)
-        return render(request, self.template_name, {'form': form, 'langs': langs.langs})
+        if self.lang == 'None':
+            self.lang = request.user.coder.lang
+        print(self.lang)
+        form1 = self.form_class1(self.lang, None)
+        form2 = self.form_class2(None)
+        return render(request, self.template_name,
+                      {'form1': form1, 'form2': form2, 'lang': self.lang})
 
-    def post(self,request):
-        lang = request.POST['lang']
-        if lang!=None:
-            form = self.form_class(lang, None)
-            return render(request, self.template_name, {'form': form, 'langs': langs.langs})
-        form = self.form_class(request.POST['form'])
-        if not form.is_valid():
-             return redirect('ide')
-        # code to be added for handling code-data which is in form
-        # use Submission model to add this data to database
-        return render(request, self.template_name, {'form': form, 'langs': langs.langs})
+    def post(self, request):
+
+        if 'lang_select' in request.POST:
+            form2 = self.form_class2(request.POST)
+            if form2.is_valid():
+                self.lang = form2.cleaned_lang()
+                form1 = self.form_class1(self.lang, None)
+                form2 = self.form_class2(None)
+                return render(request, self.template_name,
+                              {'form1': form1, 'form2': form2, 'lang': self.lang})
+            else:
+                form1 = self.form_class1(self.lang, None)
+                form2 = self.form_class2(None)
+                return render(request, self.template_name,
+                              {'form1': form1, 'form2': form2, 'lang': self.lang})
+
+        else:
+            form1 = self.form_class1(data=request.POST)
+            if not form1.is_valid():
+                form1 = self.form_class1(self.lang, None)
+                form2 = self.form_class2(None)
+                return render(request, self.template_name,
+                              {'form1': form1, 'form2': form2, 'lang': self.lang})
+
+            #Take the form data in a file and store it in Submission model
+            return redirect('/')
